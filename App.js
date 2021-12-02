7db797a9cd2f308b5b2debe87eb120fb67b5aa7b
Abdullah2500 <abdullah.khalil0315@gmail.com>
2021-12-02 23:30:10 +0500

    app.js

--- App.js
+++ App.js
@@ -6,6 +6,3 @@ const App = () => {
 };
 
 export default App;
-// what is the difference of the two of the people, being the most important being in
-// man of the match 
-// one of the world's most important being in the most important being in the most 
