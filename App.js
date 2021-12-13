3bae6af22f5472086766132c43e7aa20d9ee89ec
abdullah2500 <abdullah.khalil0315@gmail.com>
2021-12-13 19:20:38 +0500

    First One

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import NavScreens from './navigation/NavScreens';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 const App = () => {
-  return <NavScreens />;
+  return (
+    // <Provider store={store}>
+    <NavScreens />
+    // </Provider>
+  );
 };
 
 export default App;
